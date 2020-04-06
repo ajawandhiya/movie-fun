4fa4617d334a5bb6c429c5d83c468f5537a85710
ajawandhiya <abhishek.jawandhiya@cognizant.com>
2020-04-06 17:58:01 -0500

    2 datasources

---
+++ src/main/java/org/superbiz/moviefun/blobstore/S3Store.java
@@ -0,0 +1,26 @@
+package org.superbiz.moviefun.blobstore;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+
+import java.io.IOException;
+import java.util.Optional;
+
+public class S3Store implements BlobStore {
+    public S3Store(AmazonS3Client s3Client, String photoStorageBucket) {
+    }
+
+    @Override
+    public void put(Blob blob) throws IOException {
+
+    }
+
+    @Override
+    public Optional<Blob> get(String name) throws IOException {
+        return Optional.empty();
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+}
